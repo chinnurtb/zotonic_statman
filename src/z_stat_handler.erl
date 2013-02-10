33a7adeb5cd4c86eb94d2ef866dcf36c0da16351
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-02-10 15:56:00 +0100

    Api change

--- src/z_stat_handler.erl
+++ src/z_stat_handler.erl
@@ -17,20 +17,15 @@ init() ->
    
 % @doc
 % 
-update(#counter{name=Name, op=Op, value=V}, #stats_from{host=Host, system=System}) ->
-    case Op of 
-        'incr' ->
-            statman_counter:incr({Name, Host, System}, V);
-        'decr' ->
-            statman_counter:incr({Name, Host, System}, V);
-        'set' ->
-            statman_counter:set({Name, Host, System}, V)
-    end;
+
+% counter.
+update(#counter{name=Name, op=incr, value=Value}, #stats_from{host=Host, system=System}) ->
+    statman_counter:incr({Name, Host, System}, Value);
+update(#counter{name=Name, op=decr, value=Value}, #stats_from{host=Host, system=System}) ->
+    statman_counter:incr({Name, Host, System}, Value);
+update(#counter{name=Name, op=clear, value=Value}, #stats_from{host=Host, system=System}) ->
+    statman_counter:set({Name, Host, System}, 0);
+
+% histogram.
 update(#histogram{name=Name, value=V}, #stats_from{host=Host, system=System}) ->
-    statman_histogram:record_value({Name, Host, System}, V);
-update(_Stat, []) ->
-    ok;
-update(Stat, [H|T]) ->
-    update(Stat, H),
-    update(Stat, T).
-    
+    statman_histogram:record_value({Name, Host, System}, V).
